e66c3c919b8c950d7467d3b04bc3540500503ba8
dtuanvnn <dtuanvnn@gmail.com>
2015-04-23 23:59:52 +0700

    UI: create basic UI for  search form

--- GisVN/MainWindow.xaml.cs
+++ GisVN/MainWindow.xaml.cs
@@ -24,5 +24,38 @@ namespace GisVN
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Create tab item.
+        /// </summary>
+        /// <param name="header">The header text.</param>
+        private void CreateTabItem<T>(string header)
+        {
+            var tabItem = new FabTab.FabTabItem();
+            var instance = Activator.CreateInstance<T>();
+            tabItem.Header = header;
+            tabItem.Content = instance;
+            this.tabControl.Items.Add(tabItem);
+        }
+
+        /// <summary>
+        /// Action input menu.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The route event arguments.</param>
+        private void mInput_Click(object sender, RoutedEventArgs e)
+        {
+            CreateTabItem<SampleView>("Nhập dữ liệu");
+        }
+
+        /// <summary>
+        /// Action search menu.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The route event arguments.</param>
+        private void mSearch_Click(object sender, RoutedEventArgs e)
+        {
+            CreateTabItem<SearchGiay>("Tìm kiếm dữ liệu");
+        }
     }
 }
